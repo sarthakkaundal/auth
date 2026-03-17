d3f8eab0ea268e3172a32f3322fbbae970542f17
Sarthak <skkaundal9314@gmail.com>
2026-03-17 10:55:09 +0530

    db connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
