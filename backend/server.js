dc26200bb098a1b58bf9adbde9d0059441c1c24d
Sarthak <skkaundal9314@gmail.com>
2026-03-17 14:31:37 +0530

    signup login successful

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("server is running");
